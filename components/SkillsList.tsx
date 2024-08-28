1c56f6b7f3bc4d744780f65f30ed60353364b6d9
Paul Reichetanz <paul@reichetanz.de>
2024-08-28 09:49:29 +0200

    add SkillsList component

---
+++ components/SkillsList.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export default function SkillsList() {
+  return (
+    <section>
+      <h2>Meine Fähigkeiten</h2>
+      <StyledList>
+        <li>JavaScript ES6</li>
+        <li>React</li>
+        <li>Next.js</li>
+        <li>Git</li>
+        <li>HTML & CSS</li>
+        <li>Styled-Components</li>
+        <li>Jest</li>
+        <li>React Testing Library</li>
+        <li>Scrum</li>
+      </StyledList>
+    </section>
+  );
+}
+
+const StyledList = styled.ul`
+  margin-top: 1rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: 1rem;
+  list-style: circle;
+  @media (max-width: 400px) {
+    display: block;
+  }
+  li {
+    margin-left: 2rem;
+    font-weight: 600;
+  }
+  li::marker {
+    color: var(--color-tertiary);
+  }
+`;
