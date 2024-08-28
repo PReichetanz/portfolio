10446c56c8a334650b0b61e8213d6e017af1a11c
Paul Reichetanz <paul@reichetanz.de>
2024-08-28 13:41:20 +0200

    add projects

---
+++ components/Project.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { ReactElement } from "react";
+import styled from "styled-components";
+
+type ProjectType = {
+  name: string;
+  description: string;
+  imageURL: string;
+  deploymentURL: string;
+  alt?: string;
+};
+
+export default function Project({
+  name,
+  description,
+  imageURL,
+  deploymentURL,
+  alt,
+}: ProjectType): ReactElement {
+  return (
+    <ProjectWrapper>
+      <StyledImage
+        src={imageURL}
+        alt={alt || ""}
+        fill
+        sizes="(min-width: 80px) 50vw, 100vw"
+      />
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <a href={deploymentURL} target="blank">
+        Projekt ansehen &gt;
+      </a>
+    </ProjectWrapper>
+  );
+}
+
+const ProjectWrapper = styled.article`
+  position: relative;
+  img {
+    box-shadow: var(--shadow-elevation-medium);
+  }
+`;
+
+const StyledImage = styled(Image)`
+  position: relative !important;
+  display: block;
+  object-fit: contain;
+`;
---
+++ components/ProjectOverview.tsx
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+import Project from "./Project";
+
+export default function ProjectOverview() {
+  const projects = [
+    {
+      id: 1,
+      name: "Flachwitzgenerator",
+      description:
+        "Eine App, die einen zufällig ausgewählten Flachwitz anzeigt.",
+      imageURL: "/joke.png",
+      deploymentURL: "https://wordplay-nu.vercel.app",
+      githubURL: "https://github.com/PReichetanz/joke-of-the-day",
+      alt: "Startbildschirm der Flachwitz-App",
+    },
+    {
+      id: 2,
+      name: "Beurteilungsbuddy",
+      description:
+        "Eine App, die Lehrer:innen die Bewertung ihrer Schüler:innen erleichtert..",
+      imageURL: "/beurteilungsbuddy-home.png",
+      deploymentURL: "https://beurteilungsbuddy.vercel.app",
+      githubURL: "https://github.com/PReichetanz/beurteilungsbuddy",
+      alt: "Startbildschirm der Beurteilungsbuddy-App",
+    },
+    {
+      id: 3,
+      name: "Star Wars Battle Counter",
+      description: "Diese App hilft dabei, den Spielstand zu tracken.",
+      imageURL: "/swbc-home.png",
+      deploymentURL: "https://star-wars-battles-counter.vercel.app",
+      githubURL: "https://github.com/PReichetanz/star-wars-battles-counter",
+      alt: "Startbildschirm der Flachwitz-App",
+    },
+  ];
+
+  return (
+    <>
+      <StyledHeading>Woran ich gearbeitet habe</StyledHeading>
+      <StyledList role="list">
+        {projects.map(
+          ({ id, name, description, imageURL, alt, deploymentURL }) => (
+            <li key={id}>
+              <Project
+                name={name}
+                description={description}
+                imageURL={imageURL}
+                deploymentURL={deploymentURL}
+                alt={alt}
+              />
+            </li>
+          )
+        )}
+      </StyledList>
+    </>
+  );
+}
+
+const StyledHeading = styled.h2`
+  margin-top: 2rem;
+`;
+
+const StyledList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 350px));
+  gap: 2rem;
+  position: relative;
+  justify-content: center;
+  margin: 1rem 0 2rem 0;
+
+  @media screen and (min-width: 1280px) {
+    grid-template-columns: repeat(2, 400px);
+  }
+`;
