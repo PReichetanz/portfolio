e6fd78a0daea5f087b39610e826d31edc5b29170
Paul Reichetanz <paul@reichetanz.de>
2024-08-30 10:20:05 +0200

    add metadata for pages
    - move client code to separate file in order to use the Nextjs Metadata API

---
+++ app/page.client.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Contact from "@/components/Contact";
+import SkillsList from "@/components/SkillsList";
+import Introduction from "@/components/Introduction";
+import styled from "styled-components";
+import ProjectOverview from "@/components/ProjectOverview";
+import { projects } from "../lib/db";
+
+export default function HomeClient() {
+  return (
+    <>
+      <Header />
+      <MainFlexbox>
+        <Introduction />
+        <hr />
+        <SkillsList />
+        <hr />
+        <ProjectOverview title="Woran ich gearbeitet habe" projects={projects} isHomepage/>
+        <hr />
+        <Contact />
+      </MainFlexbox>
+      <Footer />
+    </>
+  );
+}
+
+const MainFlexbox = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 0 5vw;
+`;
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,13 @@
-"use client";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import Contact from "@/components/Contact";
-import SkillsList from "@/components/SkillsList";
-import Introduction from "@/components/Introduction";
-import styled from "styled-components";
-import ProjectOverview from "@/components/ProjectOverview";
-import { projects } from "../lib/db";
+import HomeClient from "./page.client";
+
+export const metadata = {
+  title: {
+    default: "Portfolio",
+    template: '%s | PAUL'
+  },
+  description: "Portfolio der Frontendprojekte von Paul Reichetanz",
+};
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <MainFlexbox>
-        <Introduction />
-        <hr />
-        <SkillsList />
-        <hr />
-        <ProjectOverview title="Woran ich gearbeitet habe" projects={projects} isHomepage/>
-        <hr />
-        <Contact />
-      </MainFlexbox>
-      <Footer />
-    </>
-  );
+  return <HomeClient />;
 }
-
-const MainFlexbox = styled.main`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  padding: 0 5vw;
-`;
